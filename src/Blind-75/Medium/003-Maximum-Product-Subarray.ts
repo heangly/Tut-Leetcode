9a5e2dd40e235f16ae6cf0f2b70893ca213202e5
Heang Ly <heanglypa@gmail.com>
2023-04-14 14:41:55 -0400

    update

--- src/Blind-75/Medium/003-Maximum-Product-Subarray.ts
+++ src/Blind-75/Medium/003-Maximum-Product-Subarray.ts
@@ -1,16 +1,16 @@
 const maxProduct = (nums: number[]): number => {
-  let prevMax = nums[0]
-  let prevMin = nums[0]
+  let maxSofar = nums[0]
+  let minSofar = nums[0]
   let result = nums[0]
 
   for (let i = 1; i < nums.length; i++) {
-    let currMax = Math.max(nums[i] * prevMax, nums[i], nums[i] * prevMin)
-    let currMin = Math.min(nums[i] * prevMin, nums[i], nums[i] * prevMax)
+    let currMax = Math.max(nums[i] * maxSofar, nums[i], nums[i] * minSofar)
+    let currMin = Math.min(nums[i] * maxSofar, nums[i], nums[i] * minSofar)
 
-    prevMax = currMax
-    prevMin = currMin
+    maxSofar = currMax
+    minSofar = currMin
 
-    result = Math.max(currMax, result)
+    result = Math.max(maxSofar, result)
   }
 
   return result
